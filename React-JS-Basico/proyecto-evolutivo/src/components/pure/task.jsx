62349ddbfcadb566c95e92b345bd0bfaafabf251
Tomas <tomassaidquinteros@gmail.com>
2023-06-24 04:36:28 -0300

    styling task

--- React-JS-Basico/proyecto-evolutivo/src/components/pure/task.jsx
+++ React-JS-Basico/proyecto-evolutivo/src/components/pure/task.jsx
@@ -2,20 +2,8 @@ import PropTypes from 'prop-types'
 import {Task} from '../../models/task.class'
 import { LEVELS } from '../../models/levels.enum'
 
-const COLORS = {
-  yellow: 'bg-yellow-400',
-  red: 'bg-red-400',
-  blue: 'bg-blue-400'
-}
-
+const styleTDparraf = 'w-fit p-[0.2rem] rounded-md text-white'
 function TaskComponent({task}) {
-
-  const difficulty = () => {
-    if (task.level === LEVELS.NORMAL) return COLORS.blue
-    if (task.level === LEVELS.URGENT) return COLORS.red
-    if (task.level === LEVELS.BLOCKING) return COLORS.BLOCKING
-  }
-  console.log(difficulty)
   return (
     <tr className='grid grid-cols-4 p-4 '>
       <td>
@@ -29,7 +17,7 @@ function TaskComponent({task}) {
         </p>
       </td>
       <td className='mx-auto'>
-        <p className={ task.level === LEVELS.NORMAL ? 'bg-blue-600 w-fit p-[0.2rem] rounded-md ' : task.level === LEVELS.BLOCKING ? 'bg-yellow-600 w-fit p-[0.2rem] rounded-md ' : 'bg-red-600 w-fit p-[0.2rem] rounded-md ' }>
+        <p className={ task.level === LEVELS.NORMAL ? `bg-blue-600 ${styleTDparraf}` : task.level === LEVELS.BLOCKING ? `bg-yellow-600 ${styleTDparraf}` : `bg-red-600 ${styleTDparraf}` }>
           { task.level }
         </p>
       </td>
