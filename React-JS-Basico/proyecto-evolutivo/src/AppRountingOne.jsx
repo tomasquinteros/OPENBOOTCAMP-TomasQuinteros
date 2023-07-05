e8e65b604953b28901d804e2c08e959ea32eeda7
Tomas <tomassaidquinteros@gmail.com>
2023-07-04 22:41:50 -0300

    añadimos react-router y sus components

---
+++ React-JS-Basico/proyecto-evolutivo/src/AppRountingOne.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import NotFoundPage from './pages/404/NotFoundPage'
+import AboutPage from './pages/about-faqs/AboutPage'
+
+function MultiPath (element, ...paths) {
+    // ...paths => Se utiliza para crear
+    // Creamos un multi path => cada path de los que ingresamos van a dirigirse a la misma pagina.
+    return paths.map(path => <Route key={path} path={path} element={ element }/>)
+}
+
+
+export default function AppRountingOne() {
+    return (
+        <BrowserRouter>
+            <div>
+                <aside>
+                    <Link to={'/'}> |HOME| </Link>
+                    <Link to={'/about'}> |ABOUT| </Link>
+                    <Link to={'/faqs'} > |FAQs| </Link>
+                </aside>
+                <main>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        {MultiPath(<AboutPage/>, '/about', 'faqs')}
+                        {/* 404 - NOT FOUND!! */}
+                        <Route path='*' element={<NotFoundPage />} />
+                    </Routes>
+                </main>
+            </div>
+
+        </BrowserRouter>
+    )
+}
--- React-JS-Basico/proyecto-evolutivo/src/pages/404/NotFoundPage.jsx
+++ React-JS-Basico/proyecto-evolutivo/src/pages/404/NotFoundPage.jsx
@@ -2,6 +2,6 @@ export default function NotFoundPage() {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            </div>
+        </div>
     )
 }
